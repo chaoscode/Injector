8aa4ae77356092a90dbeb223966d7607d7cae642
Chaoscode <Chaoscode@gmail.com>
2015-02-15 12:48:21 -0500

    int commit

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ProcessList = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ProcessIDBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DLLListBox = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ProcessList
+            // 
+            this.ProcessList.FormattingEnabled = true;
+            this.ProcessList.Location = new System.Drawing.Point(12, 25);
+            this.ProcessList.Name = "ProcessList";
+            this.ProcessList.Size = new System.Drawing.Size(120, 550);
+            this.ProcessList.TabIndex = 0;
+            this.ProcessList.SelectedIndexChanged += new System.EventHandler(this.ProcessList_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 621);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Refresh List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ProcessIDBox
+            // 
+            this.ProcessIDBox.Location = new System.Drawing.Point(12, 586);
+            this.ProcessIDBox.Name = "ProcessIDBox";
+            this.ProcessIDBox.Size = new System.Drawing.Size(120, 20);
+            this.ProcessIDBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Process List";
+            // 
+            // DLLListBox
+            // 
+            this.DLLListBox.FormattingEnabled = true;
+            this.DLLListBox.Location = new System.Drawing.Point(152, 25);
+            this.DLLListBox.Name = "DLLListBox";
+            this.DLLListBox.Size = new System.Drawing.Size(120, 550);
+            this.DLLListBox.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(152, 621);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Inject";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(152, 584);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Refresh List";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1316, 647);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.DLLListBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ProcessIDBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ProcessList);
+            this.Name = "Form1";
+            this.Text = "ChaosCode";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ProcessList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox ProcessIDBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox DLLListBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(!Directory.Exists("DLLs"))
+            {
+                Directory.CreateDirectory("DLLs");
+            }
+            FillProcessList();
+            FillDLLList();
+        }
+
+        public void FillProcessList()
+        {
+            ProcessList.Items.Clear();
+            Process[] processlist = Process.GetProcesses();
+            foreach(Process theprocess in processlist){
+                ProcessList.Items.Add(theprocess.ProcessName + " - " + theprocess.Id.ToString());                
+            }
+        }
+
+        public void FillDLLList()
+        {
+            var fileList = Directory
+                .EnumerateFiles("DLLs", "*.dll", SearchOption.AllDirectories)
+                .Select(Path.GetFullPath); // <-- note you can shorten the lambda
+            DLLListBox.DataSource = fileList.ToList();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FillProcessList();
+        }
+
+        private void ProcessList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string spliter = ProcessList.Text;
+            int postion = spliter.IndexOf("-");
+            ProcessIDBox.Text = spliter.Remove(0, postion + 2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FillDLLList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            injector injectdll = new injector();
+            injectdll.InjectByProcessID(DLLListBox.Text,Convert.ToInt32(ProcessIDBox.Text));
+        }
+    }
+}
